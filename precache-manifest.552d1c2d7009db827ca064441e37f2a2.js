e0469760873bb7910a1ada7e29c60f422ea47e3d
Caleb Zimmerman <54634315+CalebZim@users.noreply.github.com>
2020-05-13 17:16:55 -0400

    Updates

---
+++ precache-manifest.552d1c2d7009db827ca064441e37f2a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e376ac2d0dda8c25a73a9e1021db45f5",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "a58afc198fc7336ef9c5",
+    "url": "/covid-19-tracker/static/css/main.2ac83aef.chunk.css"
+  },
+  {
+    "revision": "ce3cfec91ad107296149",
+    "url": "/covid-19-tracker/static/js/2.2f3630f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19-tracker/static/js/2.2f3630f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a58afc198fc7336ef9c5",
+    "url": "/covid-19-tracker/static/js/main.a26e8f1a.chunk.js"
+  },
+  {
+    "revision": "7eea9d33a0fade3024ee",
+    "url": "/covid-19-tracker/static/js/runtime-main.73872add.js"
+  }
+]);
